fa7dd18812eda749d813546c0f36a78e95606247
jurgenwigg <jurgenwigg@protonmail.com>
2023-01-02 20:56:11 +0100

    first test case implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 /// This function is called on panic
 #[panic_handler]
@@ -12,11 +23,20 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle] // don't mangle (change) the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
